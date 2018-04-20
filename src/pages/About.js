c5cf4b349d96828241a616fe5b0c16c63b3bd405
tianhuijuan <tianhuijuan@baoyinxiaofei.com>
2018-04-20 17:54:47 +0800

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import AboutStyle from '../styleSheets/About.css';
 
 export default class extends React.Component {
     constructor(props) {
@@ -10,6 +11,7 @@ export default class extends React.Component {
         return(
             <div>
                 <Header history={this.props.history}/>
+                <div className={AboutStyle.bg}></div>
             </div>
         )
     }
